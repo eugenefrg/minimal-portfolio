2f01ca9c2cfd1cddcb49da14ea94f4666a1ca0d1
eugenefrg <eugene261997@gmail.com>
2022-04-20 09:41:00 +0800

    fix error 303

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -36,7 +36,6 @@ const ContactForm: React.FC = () => {
     <Form
       form={form}
       data-netlify="true"
-      data-netlify-recaptcha="true"
       onFinish={handleFinish}
       layout="vertical"
       requiredMark="optional"
@@ -52,7 +51,6 @@ const ContactForm: React.FC = () => {
       <Form.Item name="message" label="Message" rules={[{ required: true }]}>
         <Input.TextArea />
       </Form.Item>
-      <div data-netlify-recaptcha="true"></div>
       <Form.Item>
         <Button type="primary" htmlType="submit">
           Submit
